ab9e49b0abe52602b1aa94ec7c8251b238f2390d
davit-purba <developerbackend6@gmail.com>
2024-11-24 09:49:13 +0700

    Update settings.py

--- deteksi_tbc/settings.py
+++ deteksi_tbc/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'views',
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'deteksi_tbc.urls'
